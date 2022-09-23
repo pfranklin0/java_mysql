7c4f51b660a1c14b9bb6b582b35f1af486d92973
Paul Franklin <paul@ubuntu.attlocal.net>
2022-09-23 02:32:02 +0000

    connects

--- App.java
+++ App.java
@@ -2,7 +2,9 @@ package pf.java_mysql;
 
 public class App
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
+        Database.connect();
+        System.out.println("ok");
     }
 }
