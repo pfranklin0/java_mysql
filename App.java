ebbf22fc66b7066cb19b6190025d0de91bc0a74a
Paul Franklin <paul@ubuntu.attlocal.net>
2022-09-26 19:43:37 +0000

    database runs queries

--- App.java
+++ App.java
@@ -5,6 +5,7 @@ public class App
     public static void main(String[] args) throws Exception
     {
         Database database = Database.connect();
+        int sum = database.sumInt("id", "data");
         database.disconnect();
         System.out.println("ok");
     }
