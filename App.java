ae9bf2546ab98f2335465d79bb78b57e0cfaa917
Paul Franklin <paul@ubuntu.attlocal.net>
2022-09-26 18:30:07 +0000

    database needs to disconnect when finished with queries

--- App.java
+++ App.java
@@ -5,6 +5,7 @@ public class App
     public static void main(String[] args) throws Exception
     {
         Database database = Database.connect();
+        database.disconnect();
         System.out.println("ok");
     }
 }
